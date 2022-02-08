14bf9f2617d8523836ca9c4f50c9b2f37716339a
znk21 <vincentbruna.oficio@gmail.com>
2022-02-08 02:20:19 -0300

    Funcionalidad activa

---
+++ functions.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $("#1").click(function () {
+        $("#Selector").text("El color es #C47814");
+        $("#Circulo").css("background-color", "#C47814");
+    });
+})
+$(document).ready(function () {
+    $("#2").click(function () {
+        $("#Selector").text("El color es #47B576");
+        $("#Circulo").css("background-color", "#47B576");
+    });
+})
+$(document).ready(function () {
+    $("#3").click(function () {
+        $("#Selector").text("El color es #C62A6A");
+        $("#Circulo").css("background-color", "#C62A6A");
+    });
+})
+
+
+$(document).ready(function () {
+    $("#4").click(function () {
+        $("#Selector").text("El color es #3070CE");
+        $("#Circulo").css("background-color", "#3070CE");
+    });
+})
+$(document).ready(function () {
+    $("#5").click(function () {
+        $("#Selector").text("El color es #5FB25F");
+        $("#Circulo").css("background-color", "#5FB25F");
+    });
+})
+$(document).ready(function () {
+    $("#6").click(function () {
+        $("#Selector").text("El color es #C63838");
+        $("#Circulo").css("background-color", "#C63838");
+    });
+})
